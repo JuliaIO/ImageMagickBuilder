ec5c45bfc05fa3d21fdd1916f23e52d827cbc034
SimonDanisch <sdanisch@gmail.com>
2018-07-04 21:26:39 +0200

    build with delegates

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,11 +1,12 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
-
+name = "ImageMagick"
+version = v"6.9.10-4"
 # Collection of sources required to build imagemagick
 sources = [
     "https://github.com/ImageMagick/ImageMagick6.git" =>
-    "f6d81c5682360e87ec74f55978625fea581c330e",
+    "7a09d9b3acf12742bed4fe8d8975b78c6f4acdb4",
 ]
 
 # Bash recipe for building across all platforms
@@ -39,14 +40,15 @@ platforms = [
 # The products that we will ensure are always built
 products(prefix) = [
     LibraryProduct(prefix, "libMagickWand", :libwand),
-    LibraryProduct(prefix, "libMagickCore", :libmagickcore),
-    LibraryProduct(prefix, "libMagick++", :libmagick)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    
+    "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.1/build_Zlib.v1.2.11.jl",
+    "https://github.com/SimonDanisch/LibpngBuilder/releases/download/v1.6.31/build_libpng.v1.0.0.jl",
+    "https://github.com/SimonDanisch/LibJPEGBuilder/releases/download/v9b/build_libjpeg.v9.0.0-b.jl",
+    "https://github.com/SimonDanisch/LibTIFFBuilder/releases/download/v4.0.9/build_libtiff.v4.0.9.jl"
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, "imagemagick", sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
