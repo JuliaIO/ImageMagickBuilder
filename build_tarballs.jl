d48b2ca90e2211752f270c2e0067af1a1f5e09fa
SimonDanisch <sdanisch@gmail.com>
2018-07-09 19:17:42 +0200

    update script

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -13,27 +13,41 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd ImageMagick6/
-./configure --prefix=$prefix --host=$target
+./configure --prefix=$prefix --host=$target --disable-openmp --disable-install --disable-dependency-tracking --without-frozenpaths --without-perl
 make -j${ncore}
 make install
+
+if [[ ${target} == "x86_64-apple-darwin14" ]]; then
+    echo "-- Modifying link references for ImageMagick libraries"
+    opts=""
+    # for some reason libtiff and libpng don't need help?
+    for XLIB in libMagick++-6.Q16.8 libMagickCore-6.Q16.6 libMagickWand-6.Q16.6 libjpeg.9 libz.1
+    do
+  	  opts="${opts} -change ${WORKSPACE}/destdir/lib/${XLIB}.dylib @rpath/${XLIB}.dylib"
+    done
+    for YLIB in libMagickWand-6.Q16.6 libMagickCore-6.Q16.6 libMagick++-6.Q16.8
+    do
+	  install_name_tool ${opts} ${prefix}/lib/${YLIB}.dylib
+    done
+fi
 exit
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:i686, :glibc),
+    # Linux(:i686, :glibc),
     Linux(:x86_64, :glibc),
-    Linux(:aarch64, :glibc),
-    Linux(:armv7l, :glibc, :eabihf),
-    Linux(:powerpc64le, :glibc),
-    Linux(:i686, :musl),
-    Linux(:x86_64, :musl),
-    Linux(:aarch64, :musl),
-    Linux(:armv7l, :musl, :eabihf),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64),
-    Windows(:i686),
+    # Linux(:aarch64, :glibc),
+    # Linux(:armv7l, :glibc, :eabihf),
+    # Linux(:powerpc64le, :glibc),
+    # Linux(:i686, :musl),
+    # Linux(:x86_64, :musl),
+    # Linux(:aarch64, :musl),
+    # Linux(:armv7l, :musl, :eabihf),
+    # MacOS(:x86_64),
+    # FreeBSD(:x86_64),
+    # Windows(:i686),
     Windows(:x86_64)
 ]
 
